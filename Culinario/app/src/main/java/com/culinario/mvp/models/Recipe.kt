74647a26b244ac2052b2174600f3620272aff38c
yuliya-grosheva <gros@sfedu.ru>
2024-11-21 20:09:57 +0300

    add checking for correctness

--- Culinario/app/src/main/java/com/culinario/mvp/models/Recipe.kt
+++ Culinario/app/src/main/java/com/culinario/mvp/models/Recipe.kt
@@ -1,7 +1,14 @@
 package com.culinario.mvp.models
+
 data class Recipe(
-    val name: String, //Строка, представляющая название рецепта
+    val name: String, // Строка, представляющая название рецепта
     val cookingSpeed: Int, // Скорость приготовления в минутах
     val ingredients: List<String>, // Список ингредиентов
     val hasStepByStep: Boolean // Наличие пошагового приготовления
-)
+) {
+    init {
+        require(name.isNotBlank()) { "Recipe name cannot be blank." }
+        require(cookingSpeed >= 0) { "Cooking speed must be non-negative." }
+        require(ingredients.isNotEmpty()) { "Ingredients list cannot be empty." }
+    }
+}
