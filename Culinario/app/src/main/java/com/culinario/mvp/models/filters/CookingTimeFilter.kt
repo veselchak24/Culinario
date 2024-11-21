1182369d1e055b6505cc591749ce5974a18c3632
yuliya-grosheva <gros@sfedu.ru>
2024-11-21 20:18:40 +0300

    add a correctness check

--- Culinario/app/src/main/java/com/culinario/mvp/models/filters/CookingTimeFilter.kt
+++ Culinario/app/src/main/java/com/culinario/mvp/models/filters/CookingTimeFilter.kt
@@ -1,12 +1,14 @@
-// CookingTimeFilter позволяет фильтровать список рецептов, оставляя только те,
-// которые можно приготовить с заданной максимальной скоростью.
 package com.culinario.mvp.models.filters
+
 import com.culinario.mvp.models.Recipe
+
 class CookingTimeFilter(private val maxCookingSpeed: Int) : Filter() {
+    init {
+        require(maxCookingSpeed >= 0) { "Max cooking speed must be non-negative." }
+    }
+
     override fun apply(recipes: List<Recipe>): List<Recipe> {
+        require(recipes.isNotEmpty()) { "Recipe list cannot be empty." }
         return recipes.filter { it.cookingSpeed <= maxCookingSpeed }
     }
 }
-
-
-
