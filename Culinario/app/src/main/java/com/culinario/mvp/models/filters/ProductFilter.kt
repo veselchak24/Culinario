f7e3d574e9d5bc909156dac5a315e795e815f2e3
yuliya-grosheva <gros@sfedu.ru>
2024-11-21 20:17:40 +0300

    add a correctness check

--- Culinario/app/src/main/java/com/culinario/mvp/models/filters/ProductFilter.kt
+++ Culinario/app/src/main/java/com/culinario/mvp/models/filters/ProductFilter.kt
@@ -3,18 +3,21 @@ package com.culinario.mvp.models.filters
 import com.culinario.mvp.models.Recipe
 
 class ProductFilter(private val ingredients: List<String>, private val matchAll: Boolean) : Filter() {
+    init {
+        require(ingredients.isNotEmpty()) { "Ingredients list cannot be empty." }
+        ingredients.forEach { ingredient ->
+            require(ingredient.isNotBlank()) { "Ingredient cannot be blank." }
+        }
+    }
+
     override fun apply(recipes: List<Recipe>): List<Recipe> {
-        // Проверка на пустые ингредиенты
-        if (ingredients.isEmpty()) return recipes
+        require(recipes.isNotEmpty()) { "Recipe list cannot be empty." }
 
         return recipes.filter { recipe ->
-            // Проверка на наличие ингредиентов в рецепте
             val recipeIngredients = recipe.ingredients
             if (matchAll) {
-                // Фильтрация по всем ингредиентам
                 ingredients.all { ingredient -> recipeIngredients.contains(ingredient) }
             } else {
-                // Фильтрация по хотя бы одному ингредиенту
                 ingredients.any { ingredient -> recipeIngredients.contains(ingredient) }
             }
         }
