05b85ebfa475e631194b451ccb392e5f4effb945
yuliya-grosheva <gros@sfedu.ru>
2024-11-21 19:48:04 +0300

    Add filtration

--- Culinario/app/src/main/java/com/culinario/mvp/models/filters/ProductFilter.kt
+++ Culinario/app/src/main/java/com/culinario/mvp/models/filters/ProductFilter.kt
@@ -1,9 +1,22 @@
-//Класс ProductFilter позволяет фильтровать список рецептов,
-// оставляя только те, которые содержат определенный ингредиент.
 package com.culinario.mvp.models.filters
+
 import com.culinario.mvp.models.Recipe
-class ProductFilter(private val ingredient: String) : Filter() {
+
+class ProductFilter(private val ingredients: List<String>, private val matchAll: Boolean) : Filter() {
     override fun apply(recipes: List<Recipe>): List<Recipe> {
-        return recipes.filter { it.ingredients.contains(ingredient)}
+        // Проверка на пустые ингредиенты
+        if (ingredients.isEmpty()) return recipes
+
+        return recipes.filter { recipe ->
+            // Проверка на наличие ингредиентов в рецепте
+            val recipeIngredients = recipe.ingredients
+            if (matchAll) {
+                // Фильтрация по всем ингредиентам
+                ingredients.all { ingredient -> recipeIngredients.contains(ingredient) }
+            } else {
+                // Фильтрация по хотя бы одному ингредиенту
+                ingredients.any { ingredient -> recipeIngredients.contains(ingredient) }
+            }
+        }
     }
 }
