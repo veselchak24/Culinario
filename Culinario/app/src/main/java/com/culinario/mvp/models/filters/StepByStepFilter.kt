244839e832960666ae56bebbcb20e4189edf6c2b
yuliya-grosheva <gros@sfedu.ru>
2024-11-21 20:16:24 +0300

    add a correctness check

--- Culinario/app/src/main/java/com/culinario/mvp/models/filters/StepByStepFilter.kt
+++ Culinario/app/src/main/java/com/culinario/mvp/models/filters/StepByStepFilter.kt
@@ -1,9 +1,12 @@
 //класс StepByStepFilter позволяет фильтровать рецепты по наличию
-// или отсутствию пошаговых инструкций в зависимости от значения
+// или отсутствию пошаговых инструкций в зависимости от значений
 package com.culinario.mvp.models.filters
+
 import com.culinario.mvp.models.Recipe
+
 class StepByStepFilter(private val required: Boolean) : Filter() {
     override fun apply(recipes: List<Recipe>): List<Recipe> {
+        require(recipes.isNotEmpty()) { "Recipe list cannot be empty." }
         return recipes.filter { it.hasStepByStep == required }
     }
 }
