fcf4b9e3537e41166913dbaba59bc687b7551533
yuliya-grosheva <gros@sfedu.ru>
2025-03-16 23:25:52 +0300

    add additional comments in RecipeRepository

--- Culinario/app/src/main/java/com/culinario/mvp/models/RecipeRepositoryImpl.kt
+++ Culinario/app/src/main/java/com/culinario/mvp/models/RecipeRepositoryImpl.kt
@@ -1,26 +1,28 @@
 package com.culinario.mvp.models
-
+//// Реализация интерфейса RecipeRepository.
+//// В данный момент все методы не реализованы и выбрасывают исключение NotImplementedError.
 class RecipeRepositoryImpl : RecipeRepository {
+    // Возвращает список всех рецептов.
     override fun getAllRecipes(): List<Recipe> {
         throw NotImplementedError("Not yet implemented")
     }
-
+    // Ищет рецепты по заголовку.
     override fun searchRecipesByTitle(title: String): List<Recipe> {
         throw NotImplementedError("Not yet implemented")
     }
-
+    // Ищет рецепты по ингредиентам.
     override fun searchRecipesByIngredients(ingredients: List<String>): List<Recipe> {
         throw NotImplementedError("Not yet implemented")
     }
-
+    // Ищет рецепты по времени приготовления.
     override fun searchRecipesByCookingSpeed(maxSpeed: Int): List<Recipe> {
         throw NotImplementedError("Not yet implemented")
     }
-
+    // Ищет рецепты по типу.
     override fun searchRecipesByType(type: RecipeType): List<Recipe> {
         throw NotImplementedError("Not yet implemented")
     }
-
+    // Ищет рецепты по времени приема пищи.
     override fun searchRecipesByMealTime(mealTime: String): List<Recipe> {
         throw NotImplementedError("Not yet implemented")
     }
