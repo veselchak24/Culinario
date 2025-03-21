25314e41dbf5b3bca961831a67b36fc8df124384
yuliya-grosheva <gros@sfedu.ru>
2025-03-21 16:05:20 +0300

    added a search bar

--- Culinario/app/src/main/java/com/culinario/pages/FavoriteRecipesPage.kt
+++ Culinario/app/src/main/java/com/culinario/pages/FavoriteRecipesPage.kt
@@ -11,55 +11,65 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.culinario.R
 import com.culinario.backend.LocalRecipesHandler
 import com.culinario.controls.RecipeCard
+import com.culinario.mvp.models.Recipe
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FavoriteRecipesPage() {
     LocalRecipesHandler.UpdateLocalRecipes(LocalContext.current)
 
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+    val searchQuery = remember { mutableStateOf("") }
 
-    Scaffold (
-        modifier = Modifier
-            .nestedScroll(scrollBehavior.nestedScrollConnection),
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
         topBar = {
-            LargeTopAppBar(
-                title = {
-                    Text(
-                        text = "Сохранённые",
-                        style = MaterialTheme.typography.headlineLarge
-                    )
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.background,
-                    titleContentColor = MaterialTheme.colorScheme.onBackground
-                ),
-                scrollBehavior = scrollBehavior
-            )
+            // Добавляем отступ сверху для поисковой строки
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 55.dp) // Отступ сверху
+            ) {
+                TextField(
+                    value = searchQuery.value,
+                    onValueChange = { searchQuery.value = it },
+                    placeholder = { Text("Поиск...") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal =20.dp) // Горизонтальный отступ
+                )
+            }
         }
     ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+            val recipes = LocalRecipesHandler.GetLocalRecipes(LocalContext.current)
+            val filteredRecipes = recipes.filter { recipe ->
+                recipe.name.contains(searchQuery.value, ignoreCase = true) // Предполагается, что у рецепта есть поле name
+            }
 
-        if (LocalRecipesHandler.GetLocalRecipesCount() > 0) {
-            GridOfFavorite(Modifier.padding(innerPadding))
-        }
-        else {
-            EmptyPage()
+            if (filteredRecipes.isNotEmpty()) {
+                GridOfFavorite(filteredRecipes, Modifier.padding(top = 8.dp))
+            } else {
+                EmptyPage()
+            }
         }
     }
 }
@@ -70,7 +80,7 @@ fun EmptyPage() {
         modifier = Modifier
             .fillMaxSize()
     ) {
-        Column (
+        Column(
             modifier = Modifier
                 .align(Alignment.Center)
         ) {
@@ -92,18 +102,16 @@ fun EmptyPage() {
 }
 
 @Composable
-fun GridOfFavorite(modifier: Modifier) {
-    LazyVerticalGrid (
+fun GridOfFavorite(recipes: List<Recipe>, modifier: Modifier) {
+    LazyVerticalGrid(
         columns = GridCells.Adaptive(minSize = 150.dp),
         modifier = modifier
             .fillMaxWidth()
             .padding(bottom = 75.dp),
         contentPadding = PaddingValues(10.dp)
     ) {
-        items(LocalRecipesHandler.GetLocalRecipesCount()) {
-            LocalRecipesHandler.GetLocalRecipes(LocalContext.current).forEach {
-                    item -> RecipeCard(item, Modifier.padding(5.dp))
-            }
+        items(recipes.size) { index ->
+            RecipeCard(recipes[index], Modifier.padding(5.dp))
         }
     }
 }
