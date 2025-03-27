91e09d97b7834a5b46815c32b3686455f5493dc9
Сергей Бельчиков <delhikovsr@gmail.com>
2025-03-27 18:49:32 +0300

    Исправлена ошибка дублирования рецепта на странице избранных рецептов

--- Culinario/app/src/main/java/com/culinario/pages/FavoriteRecipesPage.kt
+++ Culinario/app/src/main/java/com/culinario/pages/FavoriteRecipesPage.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.culinario.R
-import com.culinario.backend.LocalRecipesHandler
 import com.culinario.controls.RecipeCard
 import com.culinario.mvp.models.Recipe
 
