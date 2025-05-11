8b3c5d43693e8debe5d095d7c246be43c807b51c
Евгений Щетиков <shchetikovevgeniy@gmail.com>
2025-05-11 20:53:26 +0300

    Update MainScreen.kt
    new MainScreen

--- Culinario/app/src/main/java/com/culinario/screens/MainScreen.kt
+++ Culinario/app/src/main/java/com/culinario/screens/MainScreen.kt
@@ -1,25 +1,36 @@
 package com.culinario.screens
 
+import android.graphics.Bitmap
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavController
 import com.culinario.backend.DEFAULT_USER_ID
 import com.culinario.backend.PREFERENCES_LOCAL_USER_KEY
@@ -98,6 +109,8 @@ fun ContentScreen (
         0 -> HomePage()
         1 -> FavoriteRecipesPage(userRepository, recipeRepository, modifier, navController)
         2 -> UserPage(modifier, userPageViewModel, navController)
-        3 -> CameraPage(modifier)
+        3 -> CameraPage(modifier) { bitmap: Bitmap ->
+
+        }
     }
 }
