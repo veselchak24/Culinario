173a78bdede31a58f0c026783709e408680e27cf
Евгений Щетиков <shchetikovevgeniy@gmail.com>
2025-05-22 16:49:47 +0300

    delete legacy code

--- Culinario/app/src/main/java/com/culinario/backend/interfaces/IOnDeserialize.kt
+++
@@ -1,8 +0,0 @@
-package com.culinario.backend.interfaces
-
-/**
- * Интерфейс, использующийся для оповещения объекта о десериализации
- */
-interface IOnDeserialize {
-    fun onDeserialize()
-}
--- Culinario/app/src/main/java/com/culinario/backend/interfaces/IOnSerialize.kt
+++
@@ -1,9 +0,0 @@
-package com.culinario.backend.interfaces
-
-
-/**
- * Интерфейс, использующийся для оповещения объекта о сериализации
- */
-interface IOnSerialize {
-    fun onSerialize();
-}
