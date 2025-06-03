e3a2b72d1fd7bc4478b11f852109da1bb8f8ba7f
Сергей Бельчиков <delhikovsr@gmail.com>
2025-06-03 15:01:38 +0300

    fixes

--- Culinario/app/src/main/java/com/culinario/pages/RecipeCreatePage.kt
+++ Culinario/app/src/main/java/com/culinario/pages/RecipeCreatePage.kt
@@ -32,6 +32,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
@@ -278,8 +279,10 @@ fun BasicInfoCreatePage(
     }
 
     Column (
-        modifier = modifier,
+        modifier = modifier
+            .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.spacedBy(10.dp)
+
     ) {
         TextField (
             modifier = Modifier
